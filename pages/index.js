09c28f62440c17ca383680484bc8d841b01f555d
Mohammad Dohadwala <78916257+hammadweb2020@users.noreply.github.com>
2022-06-10 17:44:08 +0400

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,43 @@
 import Head from 'next/head'
 import styled from 'styled-components'
-
+import videojs  from 'video.js'
+import dynamic from "next/dynamic";
+export const flvjs = dynamic(
+  () => import("videojs-contrib-hls.js"),
+  {
+    ssr: false,
+  }
+)
+import { useRef, useEffect } from "react";
 
 
 
 
 export default function  Home() {
+  const videoRef = useRef(null);
+  useEffect(() => {
+    if (videoRef.current) {
+  const player=  videojs(videoRef.current, {
  
+        sources: [
+          {
+            src: 'https://stream.mux.com/DS00Spx1CV902MCtPj5WknGlR102V5HFkDe.m3u8',
+           type: 'application/x-mpegURL'
+          }
+        ]
+      
+      });
+      player.play()
+      player.responsive(true)
+     // player.fetchFlv({
+     //   isLive: false
+    //  }).start()
+    }
+   
+
+ });
+
+  
 
   return (
   <>
@@ -19,8 +50,9 @@ export default function  Home() {
       
       <Section>
 
-     <H1>Welcome Bluamoeba Live Portal</H1>
+     <H1>Welcome bluamoeba Live Portal</H1>
 
+<video width="900px" height="540px"  preload="auto"  controls  ref={videoRef} className="video-js video-fluid vjs-theme-fantasy" />
   
 
 
